8de5ca553e8696107ab5ab5faaa277f5dafc1426
Srinivasan Rajendran <truecaller@Srinivasans-MacBook-Pro.local>
2020-04-18 16:31:29 +0200

    adding minium scale factor and ,ax and min sizes for text from the environment size xategory to adjust for different size scales

--- NBAPlayers/Views/PlayerDetail.swift
+++ NBAPlayers/Views/PlayerDetail.swift
@@ -17,7 +17,7 @@ struct PlayerDetail: View {
             Image(player.team.imageName).resizable().aspectRatio(contentMode: .fit)
 
             Image(player.imageName).clipShape(Circle()).background(Circle().foregroundColor(.white)).overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x: 0, y: -90).padding(.bottom, -70).shadow(radius: 25)
-            Text(player.name).font(.system(size: 40)).fontWeight(.bold).padding(.bottom,30)
+            Text(player.name).font(.system(size: 80)).fontWeight(.bold).padding(.bottom,30).lineLimit(1).minimumScaleFactor(0.4)
             StatText(statName: "Age", statValue: "\(player.age)")
             StatText(statName: "Height", statValue: player.height)
             StatText(statName: "Weight", statValue: "\(player.weight)")
@@ -28,6 +28,10 @@ struct PlayerDetail: View {
 
 struct PlayerDetail_Previews: PreviewProvider {
     static var previews: some View {
-        PlayerDetail(player: players[0])
+        Group {
+            PlayerDetail(player: players[0]).environment(\.sizeCategory,.extraExtraExtraLarge).previewDevice("iPhone 11 pro")
+            PlayerDetail(player: players[0]).environment(\.sizeCategory,.extraSmall).previewDevice("iPhone 8 pro")
+        }
+
     }
 }
